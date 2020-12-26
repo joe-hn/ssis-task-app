587afb40edd1e543c5f42d4db10d88a8899022e5
joe-hn <jose.orlando.escobar.osorio@gmail.com>
2020-12-26 11:11:27 -0600

    HOMOLOGACION

--- src/app/_global.ts
+++ src/app/_global.ts
@@ -1,4 +1,5 @@
 export var _global = {
-    url: 'http://localhost:5501/api/',
+    /* HOMOLOGACION*/  url: 'http://10.30.5.2:5501/api/',
+  /* LOCAL */  /*url: 'http://localhost:5501/api/',*/
     date_sql: 'YYYYMMDD'
 }
