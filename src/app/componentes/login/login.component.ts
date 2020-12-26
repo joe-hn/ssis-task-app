8e60f2040b4fcd7dc67553eb9fb4a72eabd0db2b
joe-hn <jose.orlando.escobar.osorio@gmail.com>
2020-12-26 10:47:39 -0600

    HOMOLOGACION

--- src/app/componentes/login/login.component.ts
+++ src/app/componentes/login/login.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, Validator, Validators, ControlContainer } from '@angular/forms';
-import { Router, ActivatedRoute, Params } from '@angular/router';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormControl,
+  Validator,
+  Validators,
+  ControlContainer,
+} from "@angular/forms";
+import { Router, ActivatedRoute, Params } from "@angular/router";
 
-import { ApiLoginService } from '../../servicio/api-login.service';
-import { usuario } from '../../modelo/usuario';
+import { ApiLoginService } from "../../servicio/api-login.service";
+import { usuario } from "../../modelo/usuario";
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['../../estilos/login.css'],
-  providers: [ApiLoginService]
+  selector: "app-login",
+  templateUrl: "./login.component.html",
+  styleUrls: ["../../estilos/login.css"],
+  providers: [ApiLoginService],
 })
 export class LoginComponent implements OnInit {
   modelo: usuario;
@@ -20,30 +25,50 @@ export class LoginComponent implements OnInit {
     private _route: ActivatedRoute,
     private _router: Router
   ) {
-    this.modelo = new usuario(0, '', '', '', '', '', '', 0, '', 0, false, 0, 0, '', 0, '', '');
+    this.modelo = new usuario(
+      0,
+      "",
+      "",
+      "",
+      "",
+      "",
+      "",
+      0,
+      "",
+      0,
+      false,
+      0,
+      0,
+      "",
+      0,
+      "",
+      ""
+    );
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   onSubmit() {
     if (this.modelo.USUARIO && this.modelo.PASS) {
-      this._api.Login(this.modelo).subscribe(response => {
+      this._api.Login(this.modelo).subscribe((response) => {
         if (!response.error_estado) {
           this.modelo = response.modelo;
 
-          localStorage.setItem('_usr', JSON.stringify(response.modelo));
-          localStorage.setItem('_u', response.modelo.USUARIO);
-          localStorage.setItem('_tk', response.tk);
+          localStorage.setItem("_usr", JSON.stringify(response.modelo));
+          localStorage.setItem("_u", response.modelo.USUARIO);
+          localStorage.setItem("_tk", response.tk);
 
-          this._api.usuarioId(this.modelo.ID).subscribe(resUsuario => {
-            console.log('---- USER');
-            localStorage.setItem('_user', JSON.stringify(resUsuario.modelo));      
+          this._api.usuarioId(this.modelo.ID).subscribe((resUsuario) => {
+            console.log(
+              "---- USER",
+              localStorage.setItem("_user", JSON.stringify(resUsuario.modelo))
+            );
+            localStorage.setItem("_user", JSON.stringify(resUsuario.modelo));
           });
 
-          this._router.navigate(['/tablero']);
+          this._router.navigate(["/tablero"]);
         }
-      })
+      });
     }
   }
 }
